03c68a42f9155f601fb41af13d94cc65eadebe8f
Semyon <sema.chel12@gmail.com>
2024-05-16 20:09:37 +0300

    поднимаем серв

--- server-http/index.js
+++ server-http/index.js
@@ -3,6 +3,18 @@ import http from 'http';
 const server = http.createServer((req, res) => {
 	//хост достать
 	const url = new URL(req.url, `http://${req.headers.host}`);
+	//взять путь
+	const { pathname } = url;
+
+	//логика
+	if (req.method === 'GET' && pathname === '/hello-world') {
+		//если метод из запроса = GET и путь '//hello-world'
+		//тогда отправь строку....
+		res.end('<h1>Hello world </h1>');
+	} else {
+		res.statusCode = 404;
+		res.end('Not found');
+	}
 });
 const port = 3000;
 server.listen(port, () => {
